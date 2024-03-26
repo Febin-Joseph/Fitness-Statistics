ccefa055b78685cf9272cab92c53b94c15e96168
Febin-Joseph <febinachu123@gmail.com>
2024-03-26 14:15:46 +0530

    bottom navigation

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,24 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { mainmenu, profile, setting } from '../../constant';
 import { useNavigate } from 'react-router-dom';
 
 const Menu = () => {
-  let navigate = useNavigate
+  const navigate = useNavigate();
+  const [activeIcon, setActiveIcon] = useState(1); // Set the initial state to 1 for mainmenu
 
   useEffect(() => {
-    let links = document.getElementsByTagName("li");
-    let circle = document.getElementById("circle");
+    const links = document.getElementsByTagName("li");
+    const circle = document.getElementById("circle");
 
-    for (let link of links) {
-      link.onclick = function () {
-
-        for (let link of links) {
-          link.style.opacity = "1";
-        }
-
-        circle.style.left = (80 * this.value) + "px";
-        circle.innerHTML = this.innerHTML;
-        this.style.opacity = "0";
+    for (let i = 0; i < links.length; i++) {
+      links[i].onclick = function () {
+        const iconOffset = i * 80;
+        circle.style.left = iconOffset + "px";
+        setActiveIcon(i);
       };
     }
   }, []);
@@ -26,16 +22,15 @@ const Menu = () => {
   return (
     <div className='bg-[#2B303D] flex h-[70px] w-[80%] items-center justify-center
                       rounded-t-[10px] rounded-b-[50px] relative'>
-      <ul className='flex gap-24 z-10'>
-        <li><img src={setting} alt="setting" className='size-8' /></li>
-        <li><img src={mainmenu} alt="setting" className='size-8 opacity-0' /></li>
-        <li><img src={profile} alt="setting" className='size-8' onClick={() => navigate('/profile')}/></li>
+      <ul className='flex gap-40 z-10 relative'>
+        <li className={activeIcon === 0 ? 'hidden' : ''}><img src={setting} alt="setting" className='size-8' /></li>
+        <li className={activeIcon === 1 ? 'hidden' : ''}><img src={mainmenu} alt="mainmenu" className='size-8' /></li>
+        <li className={activeIcon === 2 ? 'hidden' : ''}><img src={profile} alt="profile" className='size-8' onClick={() => navigate('/profile')} /></li>
       </ul>
       <div id='circle' className='absolute items-center justify-center flex bg-red-500
                                  w-[70px] h-[70px] rounded-[50%] border-[7px] border-bgBlue-10
                                  top-[-35px] left-[calc(50% - 35px)]'>
-
-        <img src={mainmenu} alt="setting" className='size-8' />
+        <img src={activeIcon === 0 ? setting : activeIcon === 1 ? mainmenu : profile} alt="icon" className='size-8' />
       </div>
     </div>
   );
